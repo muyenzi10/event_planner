4767500d95a62dc74fc106eea23a06326c6c0262
muyenzi10 <muyenzija10@gmail.com>
2025-04-27 17:03:50 +0200

    updated

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const router = require('./routers/routers');
 const decorouter = require("./routers/packagerouter");
+const mongoose = require('mongoose'); // Use raw mongoose here
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../frontend')));
@@ -11,18 +12,29 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(router);
 app.use(decorouter);
 
-// Catch 404 and forward to error handler
+// 404 handler
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, '../frontend/error.html'));
 });
 
-// Error handler (optional)
+// General error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Internal Server Error');
 });
 
-const PORT = 8000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Connect to MongoDB and then start server
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("MongoDB connected");
+    const PORT = 8000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err.message);
 });
